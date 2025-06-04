1acd76214b07ea7281c3d92b8418a210629052b8
DEVQamaRSulTaN <qamarsultan312278@gmail.com>
2025-06-04 09:24:26 +0500

    n

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,11 @@
+'''
+Django management command to wait for the database to be ready.
+'''
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    '''
+    Djnago command for wait for database
+    '''
+    def handle(self, *args, **options):
+        pass
