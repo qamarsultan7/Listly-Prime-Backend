8e0fb2abbee120f12463308a26a0af602144df7c
DEVQamaRSulTaN <qamarsultan312278@gmail.com>
2025-07-26 09:13:31 +0500

    n

---
+++ app/core/admin.py
@@ -0,0 +1,13 @@
+'''Django Admin Customization'''
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name',]
+
+admin.site.register(models.User,)
