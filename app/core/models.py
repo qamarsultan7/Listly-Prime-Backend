10fcacd735d01f38e62160a839029318395a3eb9
DEVQamaRSulTaN <qamarsultan312278@gmail.com>
2025-07-26 10:04:45 +0500

    new

--- app/core/models.py
+++ app/core/models.py
@@ -38,6 +38,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     name = models.CharField(max_length=255,)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
 
     objects = UserManager()
     USERNAME_FIELD = 'email'
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -17,3 +17,22 @@ class AdminSiteTests(TestCase):
             password='testpass123'
         )
         self.client.force_login(self.admin_user)
+
+    def test_users_list(self):
+        '''Test that users are listed on user page'''
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+        self.assertContains(res, self.admin_user.name)
+        self.assertContains(res, self.admin_user.email)
+
+    def test_edit_user_page(self):
+        '''Test that the edit user page works'''
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        '''Test that the create user page works'''
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
