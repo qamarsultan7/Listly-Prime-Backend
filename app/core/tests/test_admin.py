c29a154fc275762749f2e95561d0eac897630de6
DEVQamaRSulTaN <qamarsultan312278@gmail.com>
2025-07-26 10:08:10 +0500

    fixed issues

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -1,22 +1,25 @@
-'''Test for the admin Modifications'''
-
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 from django.test import Client
 
-
 class AdminSiteTests(TestCase):
     '''Test for the admin modifications'''
 
     def setUp(self):
-        '''Create user and client'''
+        '''Create users and client'''
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
-            email='user@example.com',
+            email='admin@example.com',
             password='testpass123'
         )
         self.client.force_login(self.admin_user)
+
+        self.user = get_user_model().objects.create_user(
+            email='user@example.com',
+            password='testpass123',
+            name='Test User'
+        )
 
     def test_users_list(self):
         '''Test that users are listed on user page'''
